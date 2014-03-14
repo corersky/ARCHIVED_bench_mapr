6c8ead5f49fb0ec5340309dc1c08ff69ac20026c
Samuel Harrold <samuel.harrold@gmail.com>
2014-03-14 16:30:06 -0500

    Edited count_words.py

--- Word_Count/count_words.py
+++ Word_Count/count_words.py
@@ -1,17 +1,43 @@
+#! /usr/bin/env python
+"""
+count_words.py
+
+Count words from a .txt file. Output to .csv.
+Example usage: $ python count_words.py ./input.txt output.csv
+
+Adapted from disco/examples/utils/count_words.py, wordcount.py, simple_innerjoin.py.
+"""
+
+import sys
+import csv
 from disco.core import Job, result_iterator
+from disco.util import kvgroup
 
-def map(line, params):
-    for word in line.split():
-        yield word, 1
+class CountWords(Job):
+    # 5 partitions for 5 slave nodes: scout02-06
+    partitions = 5
+    sort = True
 
-def reduce(iter, params):
-    from disco.util import kvgroup
-    for word, counts in kvgroup(sorted(iter)):
-        yield word, sum(counts)
+    def map(self, line, params):
+        for word in line.split():
+            yield word, 1
+
+    def reduce(self, iter, params):
+        for word, counts in kvgroup(sorted(iter)):
+            yield word, sum(counts)
 
 if __name__ == '__main__':
-    job = Job().run(input=["http://discoproject.org/media/text/chekhov.txt"],
-                    map=map,
-                    reduce=reduce)
-    for word, count in result_iterator(job.wait(show=True)):
-        print(word, count)
+    input_filename = "input.txt"
+    output_filename = "output.csv"
+    if len(sys.argv) > 1:
+        input_filename = sys.argv[1]
+        if len(sys.argv) > 2:
+            output_filename = sys.argv[2]
+
+    from count_words import CountWords
+    job = CountWords().run(input=[input_filename])
+
+    with open(output_filename, 'w') as fp:
+        writer = csv.writer(fp)
+        for word, counts in result_iterator(job.wait(show=True)):
+            writer.writerow(word, count)
