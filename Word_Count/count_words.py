665d9f1236d28fd065e83da9c6720aaad5d516e4
Samuel Harrold <samuel.harrold@gmail.com>
2014-03-14 17:03:56 -0500

    added simple_innerjoin.py, edited count_words.py

--- Word_Count/count_words.py
+++ Word_Count/count_words.py
@@ -3,9 +3,16 @@
 count_words.py
 
 Count words from a .txt file. Output to .csv.
-Example usage: $ python count_words.py ./input.txt output.csv
 
-Adapted from disco/examples/utils/count_words.py, wordcount.py, simple_innerjoin.py.
+Example usage:
+$ ddfs chunk data:inputtxt ./input.txt
+$ python count_words.py data:inputtxt output.csv
+
+Adapted from disco/examples/utils/count_words.py, wordcount.py, simple_innerjoin.py
+and http://disco.readthedocs.org/en/latest/start/tutorial_2.html
+
+TODO:
+Add help text.
 """
 
 import sys
@@ -22,9 +29,11 @@ class CountWords(Job):
         for word in line.split():
             yield word, 1
 
-    def reduce(self, iter, params):
-        for word, counts in kvgroup(sorted(iter)):
-            yield word, sum(counts)
+    def reduce(self, rows_iter, out, params):
+        # Is sorted necessary if sort = True?
+        # for word, count in kvgroup(sorted(rows_iter)):
+        for word, count in kvgroup(rows_iter):
+            out.add(word, sum(count))
 
 if __name__ == '__main__':
     input_filename = "input.txt"
@@ -34,10 +43,12 @@ if __name__ == '__main__':
         if len(sys.argv) > 2:
             output_filename = sys.argv[2]
 
+    # Necessary to import since slave noes do not have
+    # same namespace as master.
     from count_words import CountWords
     job = CountWords().run(input=[input_filename])
 
     with open(output_filename, 'w') as fp:
         writer = csv.writer(fp)
-        for word, counts in result_iterator(job.wait(show=True)):
-            writer.writerow(word, count)
+        for word, count in result_iterator(job.wait(show=True)):
+            writer.writerow([word] + count)
