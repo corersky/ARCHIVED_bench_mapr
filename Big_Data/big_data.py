b59fd9326d90657aff430eb03685e72942329fe4
stharrold <samuel.harrold@gmail.com>
2014-04-11 16:30:01 -0500

    added download ability

--- Big_Data/big_data.py
+++ Big_Data/big_data.py
@@ -5,16 +5,27 @@ Download bz2 files from list and upload to Disco Distributed File System.
 
 import argparse
 import urllib2
+import os
+from disco.ddfs import DDFS
 
 def main(file_in="bz2_url_list.txt"):
     with open(file_in, 'r') as f_in:
         for url in f_in:
+            print "Downloading {url}".format(url=url)
             # Skip commented lines.
             if url.startswith('#'):
                 continue
-            url = urllib.urlopen(url.strip())
-            print bz2_f
-            
+            # from http://stackoverflow.com/questions/4028697/how-do-i-download-a-zip-file-in-python-using-urllib2
+            try:
+                url = url.strip()
+                file_bz2 = os.path.basename(url)
+                f_url = urllib2.urlopen(url)
+                with open(file_bz2, 'wb') as f_bz2:
+                    f_bz2.write(f_url.read())
+            except HTTPError, e:
+                print "HTTP Error:", e.code, url
+            except URLError, e:
+                print "URL Error:", e.reason, url
     return None
 
 if __name__ == '__main__':
