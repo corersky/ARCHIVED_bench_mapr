7b5d541a527128ea47aa70865e33c33b1f389b04
stharrold <samuel.harrold@gmail.com>
2014-04-12 20:05:34 -0500

    added decompression

--- Big_Data/big_data.py
+++ Big_Data/big_data.py
@@ -1,31 +1,73 @@
 #!/usr/bin/env python
 """
 Download bz2 files from list and upload to Disco Distributed File System.
+
+TODO:
+- check ddfs tag exists
+- check disco v0.4.4
+- less IO if bz2 in map reader
+- less IO if push to ddfs w/o writing files
 """
 
 import argparse
 import urllib2
 import os
+import bz2
 from disco.ddfs import DDFS
 
+def download(url, file_out=os.path.basename(url)):
+    """
+    Download a file from a URL.
+    """
+    # From http://stackoverflow.com/questions/4028697
+    # /how-do-i-download-a-zip-file-in-python-using-urllib2
+    try:
+        print "Downloading {url}".format(url=url)
+        f_url = urllib2.urlopen(url)
+        with open(file_out, 'wb') as f_out:
+            f_out.write(f_url.read())
+    except HTTPError, e:
+        print "HTTP Error:", e.code, url
+    except URLError, e:
+        print "URL Error:", e.reason, url
+    return None
+
+def decompress(file_bz2, file_out=os.path.splitext(file_bz2)[0]):
+    """
+    Decompress bz2 files.
+    """
+    base, ext = os.path.splitext(file_bz2)
+    if ext != 'bz2':
+        print file_bz2
+        raise NameError("File extension not 'bz2'.")
+    with bz2.BZ2File(file_bz2, 'rb') as f_bz2:
+        with open(file_out, 'wb') as f_out:
+            f_out.write(f_bz2.read())
+    return None
+
 def main(file_in="bz2_url_list.txt"):
+    """
+    Download bz2 files from list and upload to Disco Distributed File System.
+    """
     with open(file_in, 'r') as f_in:
         for url in f_in:
-            print "Downloading {url}".format(url=url)
             # Skip commented lines.
             if url.startswith('#'):
                 continue
-            # from http://stackoverflow.com/questions/4028697/how-do-i-download-a-zip-file-in-python-using-urllib2
-            try:
-                url = url.strip()
-                file_bz2 = os.path.basename(url)
-                f_url = urllib2.urlopen(url)
-                with open(file_bz2, 'wb') as f_bz2:
-                    f_bz2.write(f_url.read())
-            except HTTPError, e:
-                print "HTTP Error:", e.code, url
-            except URLError, e:
-                print "URL Error:", e.reason, url
+            # Remove newlines and name file from URL.
+            url = url.strip()
+            file_bz2 = os.path.basename(url)
+            download(url=url, file_out=file_bz2)
+            file_decom = os.path.splitext(file_bz2)[0]
+            decompress(file_bz2=file_bz2, file_out=file_decom)
+            # # Load data to Disco Distributed File System.
+            # # If tag exists, delete it.
+            # tag="data:big"
+            # if DDFS().exists(tag=tag):
+            # DDFS().delete(tag=tag)
+            # DDFS().chunk(tag=tag, urls=['./'+file_bz2])
+            # delete bz2 file
+            # delete decompressed file
     return None
 
 if __name__ == '__main__':
