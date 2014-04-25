30f623b7a28dfc32d015dc700f38b93f83d7673f
stharrold <samuel.harrold@gmail.com>
2014-04-25 18:30:05 -0500

    formatted print, tmp_dir args

--- big_data/load_bz2.py
+++ big_data/load_bz2.py
@@ -30,9 +30,9 @@ def download(url, file_out="download.out"):
         with open(file_out, 'wb') as f_out:
             f_out.write(f_url.read())
     except urllib2.HTTPError, e:
-        print("HTTP Error:", e.code, url)
+        print("HTTP Error:", e.code, url, file=sys.stderr)
     except urllib2.URLError, e:
-        print("URL Error:", e.reason, url)
+        print("URL Error:", e.reason, url, file=sys.stderr)
     return None
 
 def decompress_and_partition(file_bz2, file_out="decompress.out"):
@@ -44,7 +44,7 @@ def decompress_and_partition(file_bz2, file_out="decompress.out"):
     """
     (base, ext) = os.path.splitext(file_bz2)
     if ext != '.bz2':
-        print(file_bz2)
+        print(file_bz2, file=sys.stderr)
         raise NameError("File extension not '.bz2'.")
     print(
         "Decompressing and partitioning\n"
@@ -57,7 +57,7 @@ def decompress_and_partition(file_bz2, file_out="decompress.out"):
             f_out.write(b"\n")
     return None
 
-def main(file_in="bz2_url_list.txt", tmp="/scratch/sth499"):
+def main(file_in="bz2_url_list.txt", tmp_dir="/scratch/sth499"):
     """
     Download bz2 files from list and upload to Disco Distributed File System.
     """
@@ -75,15 +75,17 @@ def main(file_in="bz2_url_list.txt", tmp="/scratch/sth499"):
             if line.startswith('#'):
                 continue
 
-            # Remove newlines and name file from URL.
+            # Download bz2 file if it doesn't already exist.
+            # Remove newlines.
             url = line.strip()
-            file_bz2 = os.path.join(tmp, os.path.basename(url))
-            # Download bz2 file.
-            download(url=url, file_out=file_bz2)
+            file_bz2 = os.path.join(tmp_dir, os.path.basename(url))
+            if not os.path.isfile(file_bz2):
+                download(url=url, file_out=file_bz2)
 
-            # Decompress and partition bz2 file.
+            # Decompress and partition bz2 file if it doesn't already exist.
             file_decom = os.path.splitext(file_bz2)[0]
-            decompress_and_partition(file_bz2=file_bz2, file_out=file_decom)
+            if not os.path.isfile(file_decom):
+                decompress_and_partition(file_bz2=file_bz2, file_out=file_decom)
 
             # Load data into Disco Distributed File System.
             print("Loading into Disco:\n{file_in}".format(file_in=file_decom))
@@ -91,18 +93,20 @@ def main(file_in="bz2_url_list.txt", tmp="/scratch/sth499"):
                 DDFS().chunk(tag=tag, urls=[os.path.join('./', file_decom)])
             except ValueError:
                 print("ValueError:\n{file_decom}".format(file_decom=file_decom), file=sys.stderr)
-            # Delete bz2 and decompressed files.
+
+            # Delete bz2 and decompressed files to save space.
             print("Deleting:\n{file_bz2}\n{file_decom}".format(file_bz2=file_bz2, file_decom=file_decom))
             os.remove(file_bz2)
             os.remove(file_decom)
+
     return None
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Download bz2 files from list and upload to Disco Distributed File System.")
     parser.add_argument("--file_in", default="bz2_url_list.txt", 
                         help="Input file list of URLs to bz2 files for download. Default: bz2_url_list.txt")
-    parser.add_argument("--tmp", default="/scratch/sth499",
+    parser.add_argument("--tmp_dir", default="/scratch/sth499",
                         help="Path where to temporarily save bz2 files for extraction and loading.")
     args = parser.parse_args()
     print(args)
-    main(file_in=args.file_in, tmp=args.tmp)
+    main(file_in=args.file_in, tmp_dir=args.tmp_dir)
