0c327e2f82f879f782a59e7be8bc5b5fb6a6f6ae
stharrold <samuel.harrold@gmail.com>
2014-04-29 13:33:55 -0500

    count_words works, load works

--- count_words/count_words_map_reduce.py
+++ count_words/count_words_map_reduce.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """
-Count words from a .txt file. Output to .csv.
+Count words from a Disco tag. Output to .csv.
 
 TODO:
-- check ddfs tag exists
+- make master script to run load then count_words
 - check disco v0.4.4
 """
 
@@ -25,10 +25,10 @@ class CountWords(Job):
             out.add(word, sum(count))
         return None
 
-def main(tag_in, file_out):
+def main(tag, file_out):
     # Import since slave nodes do not have same namespace as master.
     from count_words_map_reduce import CountWords
-    job = CountWords().run(input=[tag_in], map_reader=chain_reader)
+    job = CountWords().run(input=[tag], map_reader=chain_reader)
     with open(file_out, 'w') as f_out:
         writer = csv.writer(f_out, quoting=csv.QUOTE_NONNUMERIC)
         for word, count in result_iterator(job.wait(show=True)):
@@ -37,32 +37,17 @@ def main(tag_in, file_out):
 
 if __name__ == '__main__':
 
-    file_in_default  = "input.txt"
-    tag_in_default   = "data:count_words"
+    tag_default   = "data:count_words"
     file_out_default = "output.csv"
 
-    parser = argparse.ArgumentParser(description="Count words in a file using disco with map-reduce.")
-    parser.add_argument("--file_in",
-                        default=file_in_default,
-                        help="Input file. Default: {default}".format(default=file_in_default))
-    parser.add_argument("--tag_in",
-                        default=tag_in_default,
-                        help="Input tag. Default: {default}".format(default=tag_in_default))
+    parser = argparse.ArgumentParser(description="Count words from a tagged Disco data set using map-reduce.")
+    parser.add_argument("--tag",
+                        default=tag_default,
+                        help="Input tag. Default: {default}".format(default=tag_default))
     parser.add_argument("--file_out",
                         default=file_out_default,
                         help="Output file. Default: {default}".format(default=file_out_default))
     args = parser.parse_args()
     print args
     
-    if args.file_in:
-        # TODO: Rename tag data:count_words1 if tag exists.
-        # Disco v0.4.4 requrires that ./ prefix the file to identify as a local file.
-        # http://disco.readthedocs.org/en/0.4.4/howto/chunk.html#chunking
-        try:
-            tag_in = "data:count_words"
-            DDFS().chunk(tag=tag_in, urls=["./"+file_in])
-            main(tag_in=args.tag_in, file_out=args.file_out)
-        finally:
-            DDFS().delete(tag=tag_in)
-    if args.tag_in:
-        main(tag_in=args.tag_in, file_out=args.file_out)
+    main(tag=args.tag, file_out=args.file_out)
--- count_words/load.py
+++ count_words/load.py
@@ -4,51 +4,46 @@ Load data from a .txt file to Disco.
 
 TODO:
 - check disco v0.4.4
+- make module load shared between count_words and sort.
 """
 
+from __future__ import print_function
+import sys
+import os
 import argparse
 from disco.ddfs import DDFS
 
+def load(file_in, tag):
+    # TODO: Rename tag data:count_words1 if tag exists.
+    if DDFS().exists(tag=tag):
+        print("WARNING: Overwriting Disco tag {tag}.".format(tag=tag), file=sys.stderr)
+        DDFS().delete(tag=tag)
+    # Disco v0.4.4 requrires that ./ prefix the file to identify as a local file.
+    # http://disco.readthedocs.org/en/0.4.4/howto/chunk.html#chunking
+    print("Loading into Disco:\n{file_in}\nas\n{tag}".format(file_in=file_in, tag=tag))
+    try:
+        DDFS().chunk(tag=tag, urls=[os.path.join('./', file_in)])
+    except ValueError as err:
+        print("ValueError: "+err.message, file=sys.stderr)
+    return None
 
-
-def main(tag_in, file_out):
-    # Import since slave nodes do not have same namespace as master.
-    from count_words_map_reduce import CountWords
-    job = CountWords().run(input=[tag_in], map_reader=chain_reader)
-    with open(file_out, 'w') as f_out:
-        writer = csv.writer(f_out, quoting=csv.QUOTE_NONNUMERIC)
-        for word, count in result_iterator(job.wait(show=True)):
-            writer.writerow([word, count])
+def main(file_in, tag):
+    load(file_in, tag)
     return None
 
 if __name__ == '__main__':
 
     file_in_default  = "input.txt"
-    tag_in_default   = "data:count_words"
-    file_out_default = "output.csv"
+    tag_default = "data:count_words"
 
-    parser = argparse.ArgumentParser(description="Count words in a file using disco with map-reduce.")
+    parser = argparse.ArgumentParser(description="Load data from a file into Disco and tag.")
     parser.add_argument("--file_in",
                         default=file_in_default,
                         help="Input file. Default: {default}".format(default=file_in_default))
-    parser.add_argument("--tag_in",
-                        default=tag_in_default,
-                        help="Input tag. Default: {default}".format(default=tag_in_default))
-    parser.add_argument("--file_out",
-                        default=file_out_default,
-                        help="Output file. Default: {default}".format(default=file_out_default))
+    parser.add_argument("--tag",
+                        default=tag_default,
+                        help="Disco tag for input file. Default: {default}".format(default=tag_default))
     args = parser.parse_args()
-    print args
-    
-    if args.file_in:
-        # TODO: Rename tag data:count_words1 if tag exists.
-        # Disco v0.4.4 requrires that ./ prefix the file to identify as a local file.
-        # http://disco.readthedocs.org/en/0.4.4/howto/chunk.html#chunking
-        try:
-            tag_in = "data:count_words"
-            DDFS().chunk(tag=tag_in, urls=["./"+file_in])
-            main(tag_in=args.tag_in, file_out=args.file_out)
-        finally:
-            DDFS().delete(tag=tag_in)
-    if args.tag_in:
-        main(tag_in=args.tag_in, file_out=args.file_out)
+    print(args)
+
+    main(file_in=args.file_in, tag=args.tag)
