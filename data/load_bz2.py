d7b49b0a5fcaa2e94ec47cf986da766bb421626b
stharrold <samuel.harrold@gmail.com>
2014-05-16 13:37:43 -0500

    reorg ddfs tagging

--- data/load_bz2.py
+++ data/load_bz2.py
@@ -12,6 +12,47 @@ import sys
 from disco.ddfs import DDFS
 import pandas as pd
 
+def create_df_concat(fin):
+    """
+    Read in a file with a list of CSV files
+    and combine the CSV files into a single dataframe.
+    """
+    df_dict = {}
+    with open(fin, 'r') as fr:
+        for line in fr:
+            if line.startswith('#'):
+                continue
+            else:
+                fcsv = line.strip()
+                (key, ext) = os.path.splitext(fcsv)
+                df = csv_to_df(fcsv=fcsv)
+                df_dict[key] = df
+    df_concat = pd.concat(df_dict)
+    return df_concat
+
+def csv_to_df(fcsv):
+    """
+    Read a CSV file and return as a dataframe.
+    Ignore comments starting with #.
+    """
+    if not os.path.isfile(fcsv):
+        raise IOError("File does not exist: {fname}".format(fname=fcsv))
+    (fcsv_base, ext) = os.path.splitext(fcsv)
+    fcsv_nocmts = fcsv_base + '_temp' + ext
+    if not ext == '.csv':
+        raise IOError("File extension not '.csv': {fname}".format(fname=fcsv))
+    with open(fcsv, 'r') as fcmts:
+        # Make a temporary file without comments.
+        with open(fcsv_nocmts, 'w') as fnocmts:
+            for line in fcmts:
+                if line.startswith('#'):
+                    continue
+                else:
+                    fnocmts.write(line)
+    df = pd.read_csv(fcsv_nocmts, sep=',')
+    os.remove(fcsv_nocmts)
+    return df
+
 def download(url, file_out="download.out"):
     """
     Download a file from a URL.
@@ -38,7 +79,7 @@ def decompress_and_partition(file_bz2, file_out="decompress.out"):
     """
     (base, ext) = os.path.splitext(file_bz2)
     if ext != '.bz2':
-        raise NameError("File extension not '.bz2':{fname}".format(fname=file_bz2))
+        raise IOError("File extension not '.bz2': {fname}".format(fname=file_bz2))
     print("Decompressing and partitioning:\n"
           +"{file_bz2}\nto\n{file_out}".format(file_bz2=file_bz2, file_out=file_out))
     # Read large file incrementally and insert newlines every 100 KB. From:
@@ -50,26 +91,55 @@ def decompress_and_partition(file_bz2, file_out="decompress.out"):
             f_out.write(b"\n")
     return None
 
-def csv_to_df(fcsv):
+def upload_to_ddfs(fin, tag):
     """
-    Read a CSV file and return as a dataframe.
-    Ignore comments starting with #.
+    Upload to Disco Distributed File System.
     """
-    if not os.path.isfile(fcsv):
-        raise IOError("File does not exist: {fname}".format(fname=fcsv), file=sys.stderr)
-    fcsv_base, ext = os.path.splitext(fcsv)
-    fcsv_nocmts = fcsv_base + '_temp' + ext
-    if not ext == '.csv':
-        raise TypeError("File is not .csv: {fname}".format(fname=fcsv), file=sys.stderr)
-    with open(fcsv, 'r') as fcmts:
-        # Make a temporary file without comments.
-        with open(fcsv_nocmts, 'w') as fnocmts:
-            for line in fcmts:
-                if line.startswith('#'):
-                    continue
-                else:
-                    fnocmts.write(line)
-    return pd.read_csv(fcsv_nocmts, sep=',')
+    if DDFS().exists(tag=tag):
+        print("INFO: Overwriting Disco tag {tag}.".format(tag=tag), file=sys.stderr)
+        DDFS().delete(tag=tag)
+    try:
+        DDFS().chunk(tag=tag, urls=[os.path.join('./', fin)])
+    except ValueError as err:
+        print("ValueError: "+err.message
+              +"File: {fin}".format(fin=fin), file=sys.stderr)
+    return None
+
+def main(fin, tmp_dir, delete, no_upload):
+    """
+    Download bz2 files and upload to Disco.
+    """
+    # Read in CSV files to dataframes to manage tags with bz2 files.
+    df_all = create_df_all(fin)
+    select_filetags = df_all['filetag'].notnull()
+    df_filetags = df_all[select_filetags]
+    for (idx, bz2url, filetag) in df_filetags[['bz2url', 'filetag']].itertuples():
+        # TODO: if not no_upload
+        # Download bz2 file if it doesn't exist.
+        file_bz2 = os.path.join(tmp_dir, os.path.basename(bz2url))
+        if os.path.isfile(file_bz2):
+            print("Skipping download. File already exists:\n{file_bz2}".format(file_bz2=file_bz2))
+        else:
+            download(url=url, file_out=file_bz2)
+        # Decompress and partition bz2 file if it doesn't exist.
+        file_decom = os.path.splitext(file_bz2)[0]
+        if os.path.isfile(file_decom):
+            print("Skipping decompress and partition. "
+                  +"File already exists:\n{file_decom}".format(file_decom=file_decom))
+        else:
+            decompress_and_partition(file_bz2=file_bz2, file_out=file_decom)
+        # Load data into Disco Distributed File System.
+        # If Disco tag exists, delete it.
+        # Don't add all-new data to an already existing tag.
+        if not no_upload:
+            print("Loading into Disco:\n{fin}\nunder tag\n{tag}".format(fin=fin, tag=tag))
+            upload_to_ddfs(fin=file_decom, tag=filetag)
+        # Delete bz2 and decompressed files.
+        if delete:
+            print("Deleting:\n{file_bz2}\n{file_decom}".format(file_bz2=file_bz2, file_decom=file_decom))
+            os.remove(file_bz2)
+            os.remove(file_decom)
+    return None
 
 # want: tags from datasets_ddfstags with data loaded
 # store datasets_ddfstags as df to ref dataset files
@@ -91,71 +161,70 @@ def csv_to_df(fcsv):
 # read data_sets_ddfs_tags.txt as dataframe dsets
 # for file in dsets, convert csv to df, read in csv
 
-####################
-# REDO BELOW HERE
+# ####################
+# # REDO BELOW HERE
 
-def main(file_in, tmp_dir, tag, delete, no_upload):
-    """
-    Download bz2 files from list and upload to Disco.
-    """
-    with open(file_in, 'r') as f_in:
-        # If Disco tag exists, delete it.
-        # Don't add all-new data to an already existing tag.
-        if DDFS().exists(tag=tag):
-            print("WARNING: Overwriting Disco tag {tag}.".format(tag=tag), file=sys.stderr)
-            DDFS().delete(tag=tag)
+# def main(fin, tmp_dir, tag, delete, no_upload):
+#     """
+#     Download bz2 files from list and upload to Disco.
+#     """
+#     with open(fin, 'r') as f_in:
+#         # If Disco tag exists, delete it.
+#         # Don't add all-new data to an already existing tag.
+#         if DDFS().exists(tag=tag):
+#             print("WARNING: Overwriting Disco tag {tag}.".format(tag=tag), file=sys.stderr)
+#             DDFS().delete(tag=tag)
 
-        for line in f_in:
+#         for line in f_in:
 
-            # Skip commented lines.
-            if line.startswith('#'):
-                continue
+#             # Skip commented lines.
+#             if line.startswith('#'):
+#                 continue
 
-            # Remove newlines and name file from URL.
-            url = line.strip()
-            file_bz2 = os.path.join(tmp_dir, os.path.basename(url))
-            # Download bz2 file if it doesn't exist.
-            if os.path.isfile(file_bz2):
-                print("Skipping download. File already exists:\n{file_bz2}".format(file_bz2=file_bz2))
-            else:
-                download(url=url, file_out=file_bz2)
+#             # Remove newlines and name file from URL.
+#             url = line.strip()
+#             file_bz2 = os.path.join(tmp_dir, os.path.basename(url))
+#             # Download bz2 file if it doesn't exist.
+#             if os.path.isfile(file_bz2):
+#                 print("Skipping download. File already exists:\n{file_bz2}".format(file_bz2=file_bz2))
+#             else:
+#                 download(url=url, file_out=file_bz2)
 
-            # Decompress and partition bz2 file if it doesn't exist.
-            file_decom = os.path.splitext(file_bz2)[0]
-            if os.path.isfile(file_decom):
-                print("Skipping decompress and partition. "
-                      +"File already exists:\n{file_decom}".format(file_decom=file_decom))
-            else:
-                decompress_and_partition(file_bz2=file_bz2, file_out=file_decom)
+#             # Decompress and partition bz2 file if it doesn't exist.
+#             file_decom = os.path.splitext(file_bz2)[0]
+#             if os.path.isfile(file_decom):
+#                 print("Skipping decompress and partition. "
+#                       +"File already exists:\n{file_decom}".format(file_decom=file_decom))
+#             else:
+#                 decompress_and_partition(file_bz2=file_bz2, file_out=file_decom)
 
-            # Load data into Disco Distributed File System.
-            if not no_upload:
-                print("Loading into Disco:\n{file_in}\nunder tag\n{tag}".format(file_in=file_decom, tag=tag))
-                try:
-                    DDFS().chunk(tag=tag, urls=[os.path.join('./', file_decom)])
-                except ValueError as err:
-                    print("ValueError: " + err.message, file=sys.stderr)
-                    print("File: {file_decom}".format(file_decom=file_decom), file=sys.stderr)
+#             # Load data into Disco Distributed File System.
+#             if not no_upload:
+#                 print("Loading into Disco:\n{fin}\nunder tag\n{tag}".format(fin=file_decom, tag=tag))
+#                 try:
+#                     DDFS().chunk(tag=tag, urls=[os.path.join('./', file_decom)])
+#                 except ValueError as err:
+#                     print("ValueError: " + err.message, file=sys.stderr)
+#                     print("File: {file_decom}".format(file_decom=file_decom), file=sys.stderr)
 
-            # Delete bz2 and decompressed files.
-            if delete:
-                print("Deleting:\n{file_bz2}\n{file_decom}".format(file_bz2=file_bz2, file_decom=file_decom))
-                os.remove(file_bz2)
-                os.remove(file_decom)
+#             # Delete bz2 and decompressed files.
+#             if delete:
+#                 print("Deleting:\n{file_bz2}\n{file_decom}".format(file_bz2=file_bz2, file_decom=file_decom))
+#                 os.remove(file_bz2)
+#                 os.remove(file_decom)
 
-    return None
+#     return None
 
 if __name__ == '__main__':
     
-    file_in_default = "bz2_url_list.txt"
+    fin_default = "csv_to_read.txt"
     tmp_dir_default = "/tmp"
-    tag_default = "data:big"
     
     parser = argparse.ArgumentParser(description="Download bz2 files from list then upload to Disco and tag.")
-    parser.add_argument("--file_in",
-                        default=file_in_default, 
+    parser.add_argument("--fin",
+                        default=fin_default, 
                         help=("Input file list of URLs to bz2 files for download. "
-                              +"Default: {default}".format(default=file_in_default)))
+                              +"Default: {default}".format(default=fin_default)))
     parser.add_argument("--tmp_dir",
                         default=tmp_dir_default,
                         help=("Path to save bz2 files for extraction and loading. "
@@ -165,10 +234,6 @@ if __name__ == '__main__':
                         help=("True/False flag to delete files after uploaded to Disco. "
                               +"Default: {default}".format(default=str(False))))
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("--tag",
-                        default=tag_default,
-                        help=("Disco tag for uploaded data. "
-                              +"Default: {default}".format(default=tag_default)))
     group.add_argument("--no_upload",
                         action="store_true",
                         help=("True/False flag to only download and decompress files, not upload to Disco. "
@@ -176,8 +241,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
 
-    main(file_in=args.file_in,
+    main(fin=args.fin,
          tmp_dir=args.tmp_dir,
-         tag=args.tag,
          delete=args.delete,
          no_upload=args.no_upload)
