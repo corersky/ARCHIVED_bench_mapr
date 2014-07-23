c9da521d283e67e44b74537730b5ffaee8f20b24
stharrold <samuel.harrold@gmail.com>
2014-07-23 18:55:18 -0500

    moving utils to utils

--- plot/utils.py
+++ plot/utils.py
@@ -3,6 +3,12 @@
 Utilities for plotting.
 """
 
+# TODO: have common module for import
+# TODO: have test module
+
+from __future__ import print_function, division
+import ast
+import datetime as dt
 import json
 
 def create_plot_config(fjson='plot_config.json'):
@@ -25,3 +31,196 @@ def create_plot_config(fjson='plot_config.json'):
     # Use indent for human readability.
     with open(fjson, 'wb') as fp:
         json.dump(setting_value, fp, sort_keys=True, indent=4)
+
+def duration_to_timedelta(duration):
+    """
+    Convert duration from HH:MM:SS to datetime timedelta object.
+    """
+    duration_arr = map(float, duration.split(':'))
+    duration_td = dt.timedelta(hours=duration_arr[0],
+                               minutes=duration_arr[1],
+                               seconds=duration_arr[2])
+    return duration_td
+
+def events_to_dict(fevents):
+    """
+    Read Disco events file from map-reduce job. Return as dict.
+    """
+    # Log files from Disco can be > 100 MB.
+    # Only read relevant portions into memory.
+    # Example record format:
+    # ['2014/03/14 17:26:14', 'scout02', 'DONE: [map:0] Task finished in 0:00:00.716']
+    # TODO: allow for chained map-reduce jobs
+    # TODO: move to disco-specific utils module
+    # Events for a single job.
+    events = {}
+    timestamp_fmt = "%Y/%m/%d %H:%M:%S"
+    with open(fevents) as fr:
+        map_started = False
+        map_finished = False
+        reduce_started = False
+        reduce_finished = False
+        for line in fr:
+            arr = ast.literal_eval(line.strip())
+            # Start job
+            if 'master' in arr[1]:
+                # ["2014/03/14 17:26:13","master","Starting job"]
+                if 'Starting job' in arr[2]:
+                    events['node_id'] = arr[1]
+                    events['time_start'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                    continue
+            # Map
+            if 'master' in arr[1]:
+                # ["2014/03/14 17:26:13","master","Starting map phase"]
+                if 'Starting map phase' in arr[2]:
+                    map_started = True
+                    events['map'] = {}
+                    events['map']['node_id'] = arr[1]
+                    events['map']['time_start'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                    continue
+                # ["2014/03/14 17:26:13","master","map:0 assigned to scout02"]
+                if (('map' in arr[2])
+                    and ('assigned to' in arr[2])):
+                    # map_id is 1st word in string, e.g. map:0
+                    # node_id is last word in string, e.g. scout02
+                    map_id = (arr[2].split(None, 1)[0]).replace(':', '_')
+                    events['map'][map_id] = {}
+                    events['map'][map_id]['node_id'] = arr[2].rsplit(None, 1)[-1]
+                    events['map'][map_id]['time_start'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                    continue
+            # ["2014/03/14 17:26:13","scout02","MSG: [map:0] Done: 2017 entries mapped"]
+            if 'master' not in arr[1]:
+                if (('MSG: [map' in arr[2])
+                    and ('Done:' in arr[2])
+                    and ('entries mapped' in arr[2])):
+                    # map_id is 2nd word in string and has [], e.g. [map:0]
+                    # num_entries is 4th word in string.
+                    arr_msg = arr[2].split()
+                    map_id = (((arr_msg[1]).replace(':', '_')).replace('[', '')).replace(']', '')
+                    num_entries = int(arr_msg[3])
+                    events['map'][map_id]['num_entries'] = num_entries
+                    continue
+                # ["2014/03/14 17:26:14","scout02","DONE: [map:0] Task finished in 0:00:00.716"]
+                if (('DONE: [map' in arr[2])
+                    and ('Task finished in' in arr[2])):
+                    # map_id is 2nd word in string and has [], e.g. [map:0]
+                    # time_elapsed is last word in string, e.g. 0:00:00.716
+                    arr_msg = arr[2].split()
+                    map_id = (((arr_msg[1]).replace(':', '_')).replace('[', '')).replace(']', '')
+                    events['map'][map_id]['time_finish'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                    events['map'][map_id]['time_elapsed'] = duration_to_timedelta(arr_msg[-1])
+                    continue
+            if 'master' in arr[1]:
+                if map_started and not map_finished:
+                    # ["2014/03/14 17:26:14","master","Starting shuffle phase"]
+                    if 'Starting shuffle phase' in arr[2]:
+                        events['map']['shuffle'] = {}
+                        events['map']['shuffle']['node_id'] = arr[1]
+                        events['map']['shuffle']['time_start'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                        continue
+                    # ["2014/03/14 17:26:14","master","Finished shuffle phase in 0:00:00.003"]
+                    if 'Finished shuffle phase' in arr[2]:
+                        # time_elapsed is last word in string, e.g. 0:00:00.716
+                        events['map']['shuffle']['time_finish'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                        events['map']['shuffle']['time_elapsed'] = duration_to_timedelta(arr[2].rsplit(None, 1)[-1])
+                        continue     
+                    # ["2014/03/14 17:26:14","master","Finished map phase in 0:00:00.844"]
+                    if 'Finished map phase' in arr[2]:
+                        map_finished = True
+                        # time_elapsed is last word in string, e.g. 0:00:00.716
+                        events['map']['time_finish'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                        events['map']['time_elapsed'] = duration_to_timedelta(arr[2].rsplit(None, 1)[-1])
+                        # Tally nodes, entries, and maps.
+                        list_nodes = []
+                        events['map']['num_entries'] = 0
+                        events['map']['num_maps'] = 0
+                        for key in events['map']:
+                            if 'map_' in key:
+                                if events['map'][key]['node_id'] not in list_nodes:
+                                    list_nodes.append(events['map'][key]['node_id'])
+                                events['map']['num_entries'] += events['map'][key]['num_entries']
+                                events['map']['num_maps'] += 1
+                        events['map']['num_nodes'] = len(list_nodes)
+                        continue
+            # Reduce
+            if 'master' in arr[1]:
+                # ["2014/03/14 17:26:14","master","Starting reduce phase"]
+                if 'Starting reduce phase' in arr[2]:
+                    reduce_started = True
+                    events['reduce'] = {}
+                    events['reduce']['node_id'] = arr[1]
+                    events['reduce']['time_start'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                    continue
+                # ["2014/03/14 17:26:14","master","reduce:0 assigned to scout02"]
+                if (('reduce' in arr[2])
+                    and ('assigned to' in arr[2])):
+                    # reduce_id is 1st word in string, e.g. reduce:0
+                    # node_id is last word in string, e.g. scout02
+                    reduce_id = (arr[2].split(None, 1)[0]).replace(':', '_')
+                    events['reduce'][reduce_id] = {}
+                    events['reduce'][reduce_id]['node_id'] = arr[2].rsplit(None, 1)[-1]
+                    events['reduce'][reduce_id]['time_start'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                    continue
+            if 'master' not in arr[1]:
+                # ["2014/03/14 17:26:14","scout02","MSG: [reduce:0] Done: 11843 entries reduced"]
+                if (('MSG: [reduce' in arr[2])
+                    and ('Done:' in arr[2])
+                    and ('entries reduced' in arr[2])):
+                    # reduce_id is 2nd word in string and has [], e.g. [reduce:0]
+                    # num_entries is 4th word in string.
+                    arr_msg = arr[2].split()
+                    reduce_id = (((arr_msg[1]).replace(':', '_')).replace('[', '')).replace(']', '')
+                    num_entries = int(arr_msg[3])
+                    events['reduce'][reduce_id]['num_entries'] = num_entries
+                    continue
+                # ["2014/03/14 17:26:15","scout02","DONE: [reduce:0] Task finished in 0:00:00.867"]
+                if (('DONE: [reduce' in arr[2])
+                    and ('Task finished in' in arr[2])):
+                    # reduce_id is 2nd word in string and has [], e.g. [reduce:0]
+                    # time_elapsed is last word in string, e.g. 0:00:00.716
+                    arr_msg = arr[2].split()
+                    reduce_id = (((arr_msg[1]).replace(':', '_')).replace('[', '')).replace(']', '')
+                    events['reduce'][reduce_id]['time_finish'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                    events['reduce'][reduce_id]['time_elapsed'] = duration_to_timedelta(arr_msg[-1])
+                    continue
+            if 'master' in arr[1]:
+                if reduce_started and not reduce_finished:
+                    # ["2014/03/14 17:26:15","master","Starting shuffle phase"]
+                    if 'Starting shuffle phase' in arr[2]:
+                        events['reduce']['shuffle'] = {}
+                        events['reduce']['shuffle']['node_id'] = arr[1]
+                        events['reduce']['shuffle']['time_start'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                        continue
+                    # ["2014/03/14 17:26:15","master","Finished shuffle phase in 0:00:00.001"]
+                    if 'Finished shuffle phase' in arr[2]:
+                        # time_elapsed is last word in string, e.g. 0:00:00.716
+                        events['reduce']['shuffle']['time_finish'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                        events['reduce']['shuffle']['time_elapsed'] = duration_to_timedelta(arr[2].rsplit(None, 1)[-1])
+                        continue     
+                    # ["2014/03/14 17:26:15","master","Finished reduce phase in 0:00:01.080"]
+                    if 'Finished reduce phase' in arr[2]:
+                        reduce_finished = True
+                        # time_elapsed is last word in string, e.g. 0:00:00.716
+                        events['reduce']['time_finish'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                        events['reduce']['time_elapsed'] = duration_to_timedelta(arr[2].rsplit(None, 1)[-1])
+                        # Tally nodes, entries, and reduces.
+                        list_nodes = []
+                        events['reduce']['num_entries'] = 0
+                        events['reduce']['num_reduces'] = 0
+                        for key in events['reduce']:
+                            if 'reduce_' in key:
+                                if events['reduce'][key]['node_id'] not in list_nodes:
+                                    list_nodes.append(events['reduce'][key]['node_id'])
+                                events['reduce']['num_entries'] += events['reduce'][key]['num_entries']
+                                events['reduce']['num_reduces'] += 1
+                        events['reduce']['num_nodes'] = len(list_nodes)
+                        continue
+            # Finish job
+            # ["2014/03/14 17:26:15","master","READY: Job finished in 0:00:01.925"]
+            if 'master' in arr[1]:
+                if 'READY: Job finished' in arr[2]:
+                    # time_elapsed is last word in string, e.g. 0:00:00.716
+                    events['time_finish'] = dt.datetime.strptime(arr[0], timestamp_fmt)
+                    events['time_elapsed'] = duration_to_timedelta(arr[2].rsplit(None, 1)[-1])
+                    continue
+    return events
