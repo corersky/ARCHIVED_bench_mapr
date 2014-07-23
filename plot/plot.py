98d2e3372fb0453950bfd07e2a6540d808bd1d9f
stharrold <samuel.harrold@gmail.com>
2014-07-23 17:47:21 -0500

    this plot works

--- plot/plot.py
+++ plot/plot.py
@@ -23,67 +23,77 @@ def create_plot_config(fjson='plot_config.json'):
     setting_value['suptitle'] = ("Platform, job_type, N nodes\n"
                                  +"exec. time vs data size")
     setting_value['xtitle']   = "Data size (GB)"
-    setting_value['xvalues']  = [1, 3, 10, 30, 100, 300, 1000]
     setting_value['ytitle']   = "Elapsed time (min)"
-    setting_value['yvalues']  = [1, 2, 4, 8, 16, 32, 64]
+    setting_value['label1']   = "Map"
+    setting_value['xypairs1'] = [(10,1), (30,2), (100,3), (300,4)]
+    # setting_value['label2']   = "Reduce"
+    # setting_value['xypairs2'] = [(10,1), (30,2), (100,4), (300,8)]
+    setting_value['label2']   = None
+    setting_value['xypairs2'] = None
     # Use binary read-write for cross-platform compatibility.
     # Use indent for human readability.
     with open(fjson, 'wb') as fp:
         json.dump(setting_value, fp, sort_keys=True, indent=4)
 
 # def plot(suptitle, xtitle, xvalues, times_map, times_reduce, fplot):
-def plot(fplot, suptitle, xtitle, xvalues, ytitle, yvalues):
+def plot(fplot, suptitle, xtitle, ytitle, label1, xypairs1, label2, xypairs2):
     """
     Plot job execution times.
     """
-    # TODO:
-    #  - if ndim yvalues = 2, make double plots
-    #  - check for labels
-    # Check inputs
-    num_xvalues = len(xvalues)
-    # TODO: Raise assertion error
-    # if len(times_map) != num_xvalues:
-    #     raise IOError(("Number of map jobs != number of x-axis values.\n"
-    #                    +" xvalues = {xvalues}\n"
-    #                    +" times_map = {times_map}").format(xvalues=xvalues,
-    #                                                        times_map=times_map))
-    # if len(times_reduce) != num_xvalues:
-    #     raise IOError(("Number of reduce jobs != number of x-axis values.\n"
-    #                    +" xvalues = {xvalues}\n"
-    #                    +" times_reduce = {times_reduce}").format(xvalues=xvalues,
-    #                                                              times_reduce=times_reduce))
-    if len(yvalues) != num_xvalues:
-        raise IOError(("Number of map jobs != number of x-axis values.\n"
-                       +" xvalues = {xvalues}\n"
-                       +" yvalues = {yvalues}").format(xvalues=xvalues,
-                                                           yvalues=yvalues))
+    # Check inputs.
     (fplot_base, ext) = os.path.splitext(fplot)
     if ext != '.pdf':
         raise IOError(("File extension not '.pdf': {fname}").format(fname=fplot))
-
     # Create figure object.
-    subplot_kw = {'xscale': 'log'}
-    fig_kw = {'figsize': (4., 6.)}
-    (fig, axes) = plt.subplots(nrows=2, ncols=1, sharex='col', subplot_kw=subplot_kw, **fig_kw)
-    # Add bar charts.
-    widths = tuple(s*0.7 for s in xvalues)
-    # TODO: redo with bars side-by-side
-    # bars_map = axes[0].bar(left=xvalues,
-    #                        height=times_map,
-    #                        width=widths,
-    #                        color='r',
-    #                        label="Map")
-    # bars_reduce = axes[0].bar(left=xvalues,
-    #                           height=times_reduce,
-    #                           width=widths,
-    #                           color='y',
-    #                           bottom=times_map,
-    #                           label="Reduce")
-    bars = axes[0].bar(left=xvalues,
-                       height=yvalues,
-                       width=widths,
-                       color='r',
-                       label="Total")
+    subplot_kw = {}
+    subplot_kw['xscale'] = 'log'
+    fig_kw = {}
+    fig_kw['figsize'] = (4., 6.)
+    (fig, ax) = plt.subplots(nrows=2, ncols=1, sharex='col', subplot_kw=subplot_kw, **fig_kw)
+    # Plot data.
+    (x1, y1) = zip(*xypairs1)
+    if xypairs2 != None:
+        (x2, y2) = zip(*xypairs2)
+        has_xypairs2 = True
+    else:
+        print("INFO: No xypairs2.")
+        has_xypairs2 = False
+    plt1_kw = {}
+    plt1_kw['xdata'] = x1
+    plt1_kw['ydata'] = y1
+    plt1_kw['color'] = 'blue'
+    plt1_kw['linestyle'] = '-'
+    plt1_kw['marker'] = 'o'
+    plt1_kw['label'] = label1
+
+    # plt1 = ax[0].semilogx(**plt1_kw)
+    # ax[1].loglog(**plt1_kw)
+
+    # plt1 = ax[0].semilogx(xdata=x1,
+    #                       ydata=y1,
+    #                       color='blue',
+    #                       linestyle='-',
+    #                       marker='o',
+    #                       label=label1)
+    # ax[1].loglog(xdata=x1,
+    #              ydata=y1,
+    #              color='blue',
+    #              linestyle='-',
+    #              marker='o')
+
+    plt1 = ax[0].semilogx(x1, y1, 'go-', label=label1)
+    ax[1].loglog(x1, y1, 'go-')
+
+    if has_xypairs2:
+        plt2_kw = {'xdata': x2,
+                   'ydata': y2,
+                   'color': 'green',
+                   'linestyle': '-',
+                   'marker': 'o',
+                   'label': label2}
+        plt2 = ax[0].semilogx(**plt2_kw)
+        ax[1].loglog(**plt2_kw)
+    # if xypairs2 != None:
     # axes[1].bar(left=xvalues,
     #             height=times_map,
     #             width=widths,
@@ -95,12 +105,7 @@ def plot(fplot, suptitle, xtitle, xvalues, ytitle, yvalues):
     #             color='y',
     #             bottom=times_map,
     #             log=True)
-    axes[1].bar(left=xvalues,
-                height=yvalues,
-                width=widths,
-                color='r',
-                log=True)
-    # Must manually set at least common ylabel manually.
+    # Set figure text.
     fig.suptitle(suptitle)
     fig.text(x=0.5,
              y=0.085,
@@ -115,25 +120,34 @@ def plot(fplot, suptitle, xtitle, xvalues, ytitle, yvalues):
              rotation='vertical')
     # Shift lower plot up to make room for legend at bottom.
     # Add metadata to legend labels.
-    box0 = axes[0].get_position()
-    axes[0].set_position([box0.x0 + 0.03,
-                          box0.y0,
-                          box0.width,
-                          box0.height])
-    box1 = axes[1].get_position()
-    axes[1].set_position([box1.x0 + 0.03,
-                          (box1.y0 + 0.04),
-                          box1.width,
-                          box1.height])
+    box0 = ax[0].get_position()
+    ax[0].set_position([box0.x0 + 0.03,
+                        box0.y0,
+                        box0.width,
+                        box0.height])
+    box1 = ax[1].get_position()
+    ax[1].set_position([box1.x0 + 0.03,
+                        (box1.y0 + 0.04),
+                        box1.width,
+                        box1.height])
     # fig.legend(handles=(bars_map, bars_reduce),
     #            labels=(bars_map.get_label(), bars_reduce.get_label()),
     #            ncol=2,
     #            loc='lower center',
     #            bbox_to_anchor=(0.05, -0.01, 0.9, 1.),
     #            mode='expand')
-    fig.legend(handles=(bars),
-               labels=(bars.get_label(), ),
-               ncol=2,
+    if has_xypairs2:
+        handles = (plt1, plt2)
+        labels = (label1, label2)
+        ncol = 2
+    else:
+        handles = (plt1)
+        # labels must be a tuple.
+        labels = (label1, )
+        ncol = 1
+    fig.legend(handles=handles,
+               labels=labels,
+               ncol=ncol,
                loc='lower center',
                bbox_to_anchor=(0.05, -0.01, 0.9, 1.),
                mode='expand')
