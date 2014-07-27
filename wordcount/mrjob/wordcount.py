208e20cdbcb389488970d97f0a6254e30a04c61d
stharrold <samuel.harrold@gmail.com>
2014-07-27 18:09:00 -0500

    finished wordcount

--- wordcount/mrjob/wordcount.py
+++ wordcount/mrjob/wordcount.py
@@ -6,8 +6,28 @@ Following https://pythonhosted.org/mrjob/guides/quickstart.html
 
 from mrjob.job import MRJob
 
-class MRWordFrequencyCount(MRJob):
+class MRWordcount(MRJob):
 
     def mapper(self, _, line):
-        yield "chars", len(line)
-        yield 
+        """
+        Read in line. Parse line into list of words.
+        Yield words with a single count.
+        """
+        words = line.split()
+        for word in words:
+            yield (word, 1)
+
+    def combiner(self, word, counts):
+        """
+        For each word, aggregate over the counts.
+        """
+        yield (word, sum(counts))
+
+    def reducer(self, word, counts):
+        """
+        For each word, aggregate over the counts.
+        """
+        yield (word, sum(counts))
+
+if __name__ == '__main__':
+    MRWordcount.run()
