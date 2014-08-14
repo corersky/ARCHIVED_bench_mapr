7da9bcb30bdaca5592e798b1b2fe1aa562ff3c9a
stharrold <samuel.harrold@gmail.com>
2014-08-13 19:31:14 -0500

    removing generated files

--- plot/utils.py
+++ plot/utils.py
@@ -373,53 +373,6 @@ def hadoop_log_to_dict(flog):
                 raise AssertionError(("Hadoop job may have failed. Check log manually."))
     return log
 
-<<<<<<< HEAD
-def dict_to_class(dobj):
-    """
-    Convert keys of a dict into attributes of a class.
-    """
-    Dclass = collections.namedtuple('Dclass', dobj.keys())
-    return Dclass(**dobj)
-
-def create_plot_config(fjson='plot_config.json'):
-    """
-    Create plot configuration file.
-    """
-    setting_value = collections.OrderedDict()
-    setting_value['fplot']     = 'plot.pdf'
-    setting_value['infodict']  = collections.OrderedDict()
-    setting_value['infodict']['Title']    = "PDF title"
-    setting_value['infodict']['Author']   = "PDF author"
-    setting_value['infodict']['Subject']  = "PDF subject"
-    setting_value['infodict']['Keywords'] = "PDF keywords"
-    setting_value['comments']  = ["Insert multiline",
-                                  "comments here."]
-    setting_value['suptitle']  = ("Platform, job description\navg. time per task vs data size")
-    setting_value['xtitle']    = "Data size (GB)"
-    setting_value['ytitle']    = "Elapsed time (min)"
-    setting_value['label1']    = "Map (num)"
-    setting_value['xypairs1']  = [(10,1), (30,2), (100,3), (300,4)]
-    setting_value['xyantn1']   = [1, 2, 3, 4]
-    setting_value['prefantn1'] = "m"
-    setting_value['label2']    = "Reduce (num)"
-    setting_value['xypairs2']  = [(10,1), (30,2), (100,4), (300,8)]
-    setting_value['xyantn2']   = [1, 1, 1, 1]
-    setting_value['prefantn2'] = "r"
-    setting_value['xmin'] = 1
-    setting_value['xmax'] = 1000
-    setting_value['ymin'] = 0.1
-    setting_value['ymax'] = 1000
-    setting_value['xdivide'] = 1
-    setting_value['ydivide'] = 1
-    setting_value['ydivbyantn'] = True
-    # Use binary read-write for cross-platform compatibility.
-    # Use indent for human readability.
-    with open(fjson, 'wb') as fp:
-        json.dump(setting_value, fp, sort_keys=False, indent=4)
-    return None
-
-=======
->>>>>>> develop
 def plot(args):
     """
     Plot job execution times.
